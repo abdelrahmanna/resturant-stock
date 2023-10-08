962ca73e8274e15791691374731d1b3ce396f3c9
abdelrahmanna <abdoayman@icloud.com>
2023-10-08 07:48:46 +0300

    created order model

---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get the products for the order.
+     */
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, "order_products")->withPivot("quantity");
+    }
+}
